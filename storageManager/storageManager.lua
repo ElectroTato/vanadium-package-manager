82fbafc0f34397e0808cfe20e368da6de617b2a5
ElectroTato <184859461+ElectroTato@users.noreply.github.com>
2026-06-21 13:07:11 +0100

    Initialize storage manager with required libraries

--- storageManager/storageManager.lua
+++ storageManager/storageManager.lua
@@ -1,1 +1,19 @@
 
+-- // Libraries
+local TatoLib = require("modules.tatolib")
+local StorageUtils = require("modules.storageUtils")
+
+-- // Main setup
+local Monitor = peripheral.wrap("right")
+
+if not Monitor then
+    print("Unable to load application, no monitors found!")
+    return
+end
+
+-- // init
+TatoLib.Init(Monitor)
+TatoLib.topbarPrint("Inventory Manager", {Height = 1})
+
+StorageUtils.checkChests()
+
